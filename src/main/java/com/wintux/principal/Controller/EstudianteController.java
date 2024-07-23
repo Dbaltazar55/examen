0aa669f5623fce6a7005d89ec5ec8f80ac43b011
Docker <Docker@DOCKERW-5TG0OE8>
2024-07-23 01:28:45 -0700

    hasta logger

--- src/main/java/com/wintux/principal/Controller/EstudianteController.java
+++ src/main/java/com/wintux/principal/Controller/EstudianteController.java
@@ -1,14 +1,34 @@
 package com.wintux.principal.Controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping(value="/estudiante")
+@RequestMapping("/estudiante")
 public class EstudianteController {
-	@GetMapping // localhost:7000/estudiante [GET]
+	
+	private static final Logger logger = LoggerFactory.getLogger(EstudianteController.class);
+	 // localhost:7000/estudiante [GET]
+	@GetMapping
+	@ResponseBody
 	public String index() {
-		return "estudiante/index";
+		logger.info("Se acaba de llamar al controlador estudiante por GET.");
+		
+		if(5%2 != 0)
+			logger.error("Se acaba de verificar que la division de 5 entre 2 no da resto 0.");
+		return "estudiante/index"; // "estudiante/index"
+		
+		// fatal, error, warn, info, debug, trace
+		
+		// TODO: agregar IP de cliente a logs
+		
+		
+		
 	}
+	
+	
 }
--- src/main/java/com/wintux/principal/Controller/Primero.java
+++ src/main/java/com/wintux/principal/Controller/Primero.java
@@ -1,5 +1,7 @@
 package com.wintux.principal.Controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,4 +16,5 @@ public class Primero {
 	public String obtenerNombre() {
 		return "Pepe Perales: " + val;
 	}
+	
 }
